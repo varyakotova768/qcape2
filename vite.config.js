48e3b27044fb7c7c148bb8b31dc5d13167e3252f
Varya <kkotovavarvara@mail.ru>
2026-06-18 21:15:20 +0300

    Изменен base на относительный './'

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/qscape2/',
+  base: './qscape2/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
