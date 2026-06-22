437ce991df526d0e95abce4c33f42fba55a2de64
Varya <kkotovavarvara@mail.ru>
2026-06-22 18:08:13 +0300

    Исправлены конфликты в package.json и vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [
-      vue(),
-      vueDevTools(),
-    ],
-    base: '/',
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      },
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  base: '/qcape2/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-    define: {
-      'import.meta.env.VITE_BITRIX_WEBHOOK': JSON.stringify(env.VITE_BITRIX_WEBHOOK)
-    }
-  }
+  },
 })
